b6298dc1710d59fb1d7f17523beb967641357e5d
Pyotr Gribanov <tridxy@gmail.com>
2024-05-02 20:56:48 +0900

    done 02.05.2024

--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -3,11 +3,12 @@ package jm.task.core.jdbc.model;
 import jakarta.persistence.*;
 
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
-    @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
     @Column(name = "name")
